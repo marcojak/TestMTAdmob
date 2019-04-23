1e7c7fc7a79fb97b6431c0de83711013cccce745
Troncone, Marco <marco.troncone@kier.co.uk>
2019-04-23 14:54:34 +0200

    Added Events

--- TestMTAdmob/TestMTAdmob/MainPage.xaml.cs
+++ TestMTAdmob/TestMTAdmob/MainPage.xaml.cs
@@ -12,15 +12,19 @@ namespace TestMTAdmob
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
-        public ObservableCollection<List<string>> MtEvents { get; set; }
+        public ObservableCollection<string> MtEvents { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
 
+            MtEvents = new ObservableCollection<string>();
+
+            MtEvents.CollectionChanged += MtEvents_CollectionChanged;
+
             //The banner inside the XAML but if you prefer you can load it here programmatically 
 
-
+            //Now we set the events (just if we want)
             CrossMTAdmob.Current.OnInterstitialLoaded += Current_OnInterstitialLoaded;
             CrossMTAdmob.Current.OnInterstitialOpened += Current_OnInterstitialOpened;
             CrossMTAdmob.Current.OnInterstitialClosed += Current_OnInterstitialClosed;
@@ -33,45 +37,61 @@ namespace TestMTAdmob
             CrossMTAdmob.Current.OnRewarded += Current_OnRewarded;
         }
 
+        private void MtEvents_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (MtEvents.Count > 0)
+                MylistView.ScrollTo(MtEvents[MtEvents.Count - 1], ScrollToPosition.End, true);
+        }
+
         private void Current_OnRewarded(object sender, MTEventArgs e)
         {
+            MtEvents.Add($"On Rewarded: {e.RewardType} - {e.RewardAmount}");
         }
 
         private void Current_OnRewardedVideoStarted(object sender, EventArgs e)
         {
+            MtEvents.Add("Rewarded Video started");
         }
 
         private void Current_OnRewardedVideoAdLeftApplication(object sender, EventArgs e)
         {
+            MtEvents.Add("Rewarded Video left application");
         }
 
         private void Current_OnRewardedVideoAdFailedToLoad(object sender, MTEventArgs e)
         {
+            MtEvents.Add("Rewarded Video Ad failed to load");
         }
 
         private void Current_OnRewardedVideoAdClosed(object sender, EventArgs e)
         {
+            MtEvents.Add("OnRewardedVideoAdClosed");
         }
 
         private void Current_OnRewardedVideoAdLoaded(object sender, EventArgs e)
         {
+            MtEvents.Add("Current_OnRewardedVideoAdLoaded");
         }
 
         private void Current_OnInterstitialClosed(object sender, EventArgs e)
         {
+            MtEvents.Add("Current_OnInterstitialClosed");
         }
 
         private void Current_OnInterstitialOpened(object sender, EventArgs e)
         {
+            MtEvents.Add("Current_OnInterstitialOpened");
         }
 
         private void Current_OnInterstitialLoaded(object sender, EventArgs e)
         {
+            MtEvents.Add("Current_OnInterstitialLoaded");
         }
 
         private void IsInterstitialLoaded(object sender, EventArgs e)
         {
             bool isLoaded = CrossMTAdmob.Current.IsInterstitialLoaded();
+            MtEvents.Add($"IsInterstitialLoaded: {isLoaded}");
         }
 
         private void LoadInterstitial(object sender, EventArgs e)
@@ -89,6 +109,7 @@ namespace TestMTAdmob
         private void IsRewardedVideoLoaded(object sender, EventArgs e)
         {
             bool isLoaded = CrossMTAdmob.Current.IsRewardedVideoLoaded();
+            MtEvents.Add($"IsRewardedVideoLoaded: {isLoaded}");
         }
 
         private void LoadRewardedVideo(object sender, EventArgs e)
